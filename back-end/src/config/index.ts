38b383881c83a277b1f6ff1c67feaec4283c3469
gia <gia6shamugia@gmail.com>
2023-12-27 18:06:34 +0400

    initalized back-end

---
+++ back-end/src/config/index.ts
@@ -0,0 +1,22 @@
+import { Config } from "../interfaces/config";
+
+const configs: {
+  development: Config;
+} = {
+  development: {
+    db: {
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    redis: {
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+    },
+    auth: {
+      secret: process.env.SECRET,
+    },
+  },
+};
